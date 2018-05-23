cc98b3654ebc1dbc1461e2fa82ce9e2418cb9b5b
Alessandra <alessandraaiello94@gmail.com>
2018-05-23 12:16:42 +0200

    Update CorpiCelesti.java

--- Algoritmi/src/CorpiCelesti.java
+++ Algoritmi/src/CorpiCelesti.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class luglio {
+public class CorpiCelesti {
 
 	public static void main(String[] args) {
 		SparseGraph<String, String> grafo = new SparseGraph<String, String>();
